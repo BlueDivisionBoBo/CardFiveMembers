d34085cfa2142ffb794cd992f501c9503812fc79
bobo <314553702@qq.com>
2016-12-10 23:35:20 +0800

    sql

--- Sell/src/main/resources/db/migration/V20161210.1__t_sell.sql
+++ Sell/src/main/resources/db/migration/V20161210.1__t_sell.sql
@@ -1,9 +1,9 @@
 CREATE TABLE `t_sell` (
   `id` int(11) NOT NULL AUTO_INCREMENT ,
-  'proxy_id' int(11)  NOT NULL COMMENT '代理人ID',
-  'player_id' varchar(50) NOT NULL COMMENT '玩家ID',
-  'game_currency_amount' varchar(50) NOT NULL COMMENT '玩家ID',
-  'sell_date' datetime NOT NULL COMMENT '出售时间',
+  `proxy_id` int(11)  NOT NULL COMMENT '代理人ID',
+  `player_id` varchar(50) NOT NULL COMMENT '玩家ID',
+  `game_currency_amount` varchar(50) NOT NULL COMMENT '玩家ID',
+  `sell_date` datetime NOT NULL COMMENT '出售时间',
   `create_date` datetime DEFAULT NULL COMMENT '创建时间',
   `create_user` varchar(50) DEFAULT NULL COMMENT '创建用户',
   `is_enable` char(1) DEFAULT '1' COMMENT '是否可用',
